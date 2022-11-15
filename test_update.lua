0ecad40bf189e7c3ebc288f6721a9378c155e8a2
gfdgdfsgsdfgsdfg432 <118283269+gfdgdfsgsdfgsdfg432@users.noreply.github.com>
2022-11-15 21:47:58 +0300

    Update test_update.lua

--- test_update.lua
+++ test_update.lua
@@ -1,4 +1,4 @@
-script_version('7.0.0')
+script_version('1.0.0')
 requests = require 'requests'
 encoding = require("encoding"); encoding.default = 'CP1251'; u8 = encoding.UTF8  
 
@@ -8,32 +8,32 @@ function main()
 	local RAW = 'https://raw.githubusercontent.com/gfdgdfsgsdfgsdfg432/gfdgdfsgsdfgsdfg432/main/ubdate.ini' -- RAW обновы
 	local r = requests.get(RAW) 
 		if r.status_code == 200 then -- проверяем status code RAW'a
-		local func,err = load('return '..r.text) -- преобразуем текст в RAW в lua массив
-		if err == nil then -- если нет ошибки
-			if thisScript().version ~= func().version then -- если версия скрипта не совпадает в обновлении
-				sampAddChatMessage('',func().version) 
-				for l in func().description:gmatch('[^\n]+') do -- выводит по строчном описание обновы(\n)
-					sampAddChatMessage("%s",u8:decode(l))
+		local func,err = load('return '..r.text) 
+		if err == nil then -- 
+			if thisScript().version ~= func().version then 
+				
+				for l in func().description:gmatch('[^\n]+') do
+					
 				end
 				
 					downloadUrlToFile(func().url, 
 						thisScript().path, 
 						function(id, status, p1, p2)
-							if status == 58 then -- если файл успешно скачан (STATUSEX_ENDDOWNLOAD)
-								sampAddChatMessage('')
+							if status == 58 then 
+								
 								thisScript():reload() 
 							else
-								sampAddChatMessage('',p1,p2)
+								
 							end
 						end
 					)
 
 			end
 		else
-			sampAddChatMessage('error %s',err)
+			
 		end
 	else
-		sampAddChatMessage('status_code',r.status_code)
+		
 	end
 	wait(-1)
 end
